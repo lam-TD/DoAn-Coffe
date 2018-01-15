891b2dcda2b586ed51c7abb0a6b06f1d1df6b43b
Trần Đức Lâm <32539012+lam-TD@users.noreply.github.com>
2018-01-16 03:22:43 +0700

    up csdl

--- DoAn-Coffe/QuanLyQuanCafe.sql
+++ DoAn-Coffe/QuanLyQuanCafe.sql
@@ -1,112 +1,139 @@
-﻿create database Cafe
+﻿create database CafeChat
 go
 
-use QuanLyQuanCafe
+use CafeChat
 go
 
 
 create table NhanVien
 (
-	id int primary key,
-	ten nvarchar(100) not null default N'Chưa nhập tên',
-	taikhoan nvarchar(100) not null ,
-	matkhau nvarchar(100) not null ,
-	quyen int not null default 0
+	nv_id int primary key,
+	nv_ten nvarchar(100) not null default N'Chưa nhập tên',
+	nv_diachi nvarchar(200),
+	nv_sdt varchar(11),
+	nv_trangthai int default 0
 )
 go
+
+create table TaiKhoan
+(
+	tk_taikhoan varchar(20) not null primary key,
+	tk_matkhau varchar(20) not null,
+	nv_id int not null,
+	tk_quyen int not null default 0
+	foreign key (nv_id) references NhanVien(nv_id)
+)
+
 create table CaTruc
 (
-	id int primary key,
-	ngaytruc datetime not null,
-	buoitruc nvarchar(100) not null,
-	id_nhanvien int not null,
-	foreign key (id_nhanvien) references NhanVien(id)
+	ct_id int primary key,
+	ct_ngaytruc datetime not null,
+	ct_buoitruc nvarchar(100) not null,
+	nv_id int not null,
+	foreign key (nv_id) references NhanVien(nv_id)
 )
 go
 
 
 create table DanhMuc
 (
-	id int primary key,
-	ten nvarchar(100) not null default N'Chưa đặt tên',
-	trangthai int default 0
+	dm_id int primary key,
+	dm_ten nvarchar(100) not null default N'Chưa đặt tên',
+	dm_trangthai int default 0
 )
 go
 
 create table ThucUong
 (
-	id int primary key,
-	ten nvarchar(100) not null default N'Chưa đặt tên',
-	gia int not null default 0,
-	trangthai int default 1,
-	id_danhmuc int not null
+	tu_id int primary key,
+	tu_ten nvarchar(100) not null default N'Chưa đặt tên',
+	tu_gia int not null default 0,
+	tu_trangthai int default 1,
+	dm_id int not null
 
-	foreign key (id_danhmuc) references DanhMuc(id)
+	foreign key (dm_id) references DanhMuc(dm_id)
 )
 go
 
 create table KhuVuc
 (
-	id int primary key,
-	ten nvarchar(100) not null,
-	trangthai nvarchar(100) default N'Đang sử dụng'
+	kv_id int primary key,
+	kv_ten nvarchar(100) not null,
+	kv_trangthai nvarchar(100) default N'Đang sử dụng'
 )
 GO
 
 create table Ban
 (
-	id int primary key,
-	ten nvarchar(100) not null default N'Bàn trống',
-	trangthai nvarchar(100) not null default N'Trống',
-	id_khuvuc int not null
+	ban_id int primary key,
+	ban_ten nvarchar(100) not null default N'Bàn trống',
+	ban_trangthai nvarchar(100) not null default N'Trống',
+	kv_id int not null
 
-	foreign key (id_khuvuc) references KhuVuc(id)
+	foreign key (kv_id) references KhuVuc(kv_id)
 )
 GO
 
 create table HoaDon
 (
-	id int primary key,
-	ngaylap datetime not null default getdate(),
-	trangthai int not null default 0,
-	tongtien int not null,
-	id_ban int not null,
-	id_nhanvien int not null
+	hd_id int primary key,
+	hd_ngaylap datetime not null default getdate(),
+	hd_trangthai int not null default 0,
+	hd_tongtien int not null,
+	ban_id int not null,
+	nv_id int not null
 
-	foreign key (id_ban) references Ban(id),
-	foreign key (id_nhanvien) references NhanVien(id)
+	foreign key (ban_id) references Ban(ban_id),
+	foreign key (nv_id) references NhanVien(nv_id)
 )
 go
 
 create table CTHD
 (
-	soluong int not null default 0,
-	thanhtien int not null, 
-	id_hoadon int not null,
-	id_thucuong int not null
+	cthd_soluong int not null default 0,
+	cthd_thanhtien int not null, 
+	hd_id int not null,
+	tu_id int not null
 
-	primary key(id_hoadon, id_thucuong),
+	primary key(hd_id, tu_id),
 
-	foreign key (id_hoadon) references HoaDon(id),
-	foreign key (id_thucuong) references ThucUong(id)
+	foreign key (hd_id) references HoaDon(hd_id),
+	foreign key (tu_id) references ThucUong(tu_id)
 )
 go
 
 
+-- Them nhanvien
+insert into NhanVien(nv_id, nv_ten, nv_diachi, nv_sdt, nv_trangthai) values ( '1',N'Nguyễn Văn A', 'Hau Giang', '42424242', '0');
+insert into NhanVien(nv_id, nv_ten, nv_diachi, nv_sdt, nv_trangthai) values ( '2', N'Nguyễn Văn b', 'hihi', '123456', '0');
 
-insert into NhanVien(id, ten, taikhoan, matkhau, quyen) values ( '1',N'Nguyễn Văn A', 'admin', 'admin', '0');
-insert into NhanVien(id, ten, taikhoan, matkhau, quyen) values ( '2', N'Nguyễn Văn b', 'hihi', '123456', '0');
-insert into CaTruc(id, ngaytruc, buoitruc, id_nhanvien) values ('1', '1-1-2018', N'Sáng', '2');
-insert into CaTruc(id, ngaytruc, buoitruc, id_nhanvien) values ('2', '1-1-2018', N'Tối', '1');
-insert into DanhMuc(id, ten) values ('1', N'Giải Khát');
-insert into DanhMuc(id, ten) values ('2', N'Sinh Tố');
-insert into ThucUong(id, ten, gia, id_danhmuc) values ('1',N'Pepsi','20000', '1');
-insert into ThucUong(id, ten, gia, id_danhmuc) values ('2',N'Sinh tố dâu','25000', '2');
-insert into KhuVuc(id, ten, trangthai) values ('1', N'Tầng 1', N'Đầy đủ');
-insert into KhuVuc(id, ten, trangthai) values ('2', N'Tầng 2', N'Đầy đủ');
-insert into Ban(id, ten, trangthai, id_khuvuc) values ('1', 'Bàn 1', '0', '1');
-insert into Ban(id, ten, trangthai, id_khuvuc) values ('2', 'Bàn 2', '0', '2');
-insert into HoaDon(id, ngaylap, tongtien, trangthai, id_ban, id_nhanvien) values ('1', '1-1-2018', '20000', '0', '1', '1');
-insert into HoaDon(id, ngaylap, tongtien, trangthai, id_ban, id_nhanvien) values ('2', '1-1-2018', '25000', '0', '2', '1');
-insert into CTHD( soluong, thanhtien, id_hoadon, id_thucuong) values ('1', '20000', '1', '1');
-insert into CTHD( soluong, thanhtien, id_hoadon, id_thucuong) values ('1', '25000', '2', '2');
+-- Them Tai Khoan
+insert into TaiKhoan values('admin','12345',1,1);
+insert into TaiKhoan values('nhanvien1','12345',1,0);
+
+-- Them Ca Truc
+insert into CaTruc(ct_id, ct_ngaytruc, ct_buoitruc, nv_id) values ('1', '1-1-2018', N'Sáng', '2');
+insert into CaTruc(ct_id, ct_ngaytruc, ct_buoitruc, nv_id) values ('2', '1-1-2018', N'Tối', '1');
+
+-- Them Danh Muc
+insert into DanhMuc(dm_id, dm_ten, dm_trangthai) values ('1', N'Giải Khát',1);
+insert into DanhMuc(dm_id, dm_ten, dm_trangthai) values ('2', N'Sinh tố',1);
+
+--Them Thuc Uong
+insert into ThucUong(tu_id, tu_ten, tu_gia, dm_id, tu_trangthai) values ('1',N'Pepsi','20000', '1',1);
+insert into ThucUong(tu_id, tu_ten, tu_gia, dm_id, tu_trangthai) values ('2',N'CoCa','20000', '1',1);
+
+-- Them Khu Vuc
+insert into KhuVuc(kv_id, kv_ten, kv_trangthai) values (1, N'Tầng 1', N'Đang sử dụng');
+insert into KhuVuc(kv_id, kv_ten, kv_trangthai) values (2, N'Tầng 1', N'Đang sử dụng');
+
+-- Them Ban
+insert into Ban(ban_id, ban_ten, ban_trangthai, kv_id) values ('1', 'Bàn 1', '1', '1');
+insert into Ban(ban_id, ban_ten, ban_trangthai, kv_id) values ('2', 'Bàn 2', '1', '1');
+-- Them Hoa Don
+insert into HoaDon(hd_id, hd_ngaylap, hd_tongtien, hd_trangthai, ban_id, nv_id) values ('1', '1-1-2018', '20000', '0', 1, '1');
+insert into HoaDon(hd_id, hd_ngaylap, hd_tongtien, hd_trangthai, ban_id, nv_id) values ('2', '1-2-2018', '200000', '0', 2, '1');
+
+--Them Chi Tiet Hoa Don
+insert into CTHD( cthd_soluong, cthd_thanhtien, hd_id, tu_id) values ('1', '20000', '1', '1');
+insert into CTHD( cthd_soluong, cthd_thanhtien, hd_id, tu_id) values ('1', '200000', '2', '1');
