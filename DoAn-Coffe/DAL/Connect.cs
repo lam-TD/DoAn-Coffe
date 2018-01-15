201bdcaea62db7b6e33952bcf54cc28bd3f91712
Trần Đức Lâm <32539012+lam-TD@users.noreply.github.com>
2018-01-16 00:36:43 +0700

    update

---
+++ DoAn-Coffe/DAL/Connect.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Connect
+    {
+        private SqlConnection conn;
+        public Connect()
+        {
+            conn = new SqlConnection(@"");
+            conn.Open();
+        }
+        public DataTable getTable(string sql)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        public bool excuteNoneQuery(string sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
